3c9a7f93a96f9ff022f106295974e50eaa40669a
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2022-05-21 14:45:54 +0900

    [Bronze I] Title: 설탕 배달, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2839. 설탕 배달/설탕 배달.py
+++ 백준/Bronze/2839. 설탕 배달/설탕 배달.py
@@ -1,23 +1,18 @@
 def sugar(n):
-    if (n < 3 or n == 4):
-        print(-1)
-        return
-    arr = [-1 for _ in range(n+1)]
-    if (n == 3):
-        print(1)
-        return
-    else:
-        arr[3] = arr[5] = 1
+    res = 0
 
-    for i in range(6, n+1):
-        min = n
-        for j in range(1, i // 2 + 1):
-            if (arr[j] != -1 and arr[i-j] != -1):
-                if (min > arr[j] + arr[i-j]):
-                    min = arr[j] + arr[i-j]
-        if (min != n):
-            arr[i] = min
-    print(arr[n])
+    while (n > 0):
+        if (n % 5 == 0):
+            res += (n // 5)
+            print(res)
+            return
+        n -= 3
+        res += 1
+        
+    if (n < 0):
+        print(-1)
+    else:
+        print(res)
 
 n = int(input())
 sugar(n)
