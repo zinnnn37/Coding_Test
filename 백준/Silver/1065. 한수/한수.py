1825fb3625a066737a5d45a2ed512df092209c87
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2022-05-24 22:37:04 +0900

    [Silver IV] Title: 한수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,22 +1,12 @@
-def check(n):
+def seq(n):
     if (n < 100):
-        return (n)
+        return n
     else:
-        if (n == 1000):
-            n -= 1
         cnt = 99
         for i in range(100, n+1):
-            cnt += sequence(i, i//10)
-        return (cnt)
-    
-def sequence(n, m):
-    if (m // 10 == 0):
-        return n%10 - m
-    else:
-        res = sequence(n//10, m//10)
-    if (res == n%10-m%10):
-        return (1)
-    else:
-        return (0)
-        
-print(check(int(input())))
+            a = list(map(int, str(i)))
+            if (a[0] - a[1] == a[1] - a[2]):
+                cnt += 1
+        return cnt
+
+print(seq(int(input())))
