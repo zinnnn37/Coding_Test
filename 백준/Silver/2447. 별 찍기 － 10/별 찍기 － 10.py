aa13af762340bf4139bf83e9dcd7aef5b8858c45
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2022-06-05 19:04:05 +0900

    [Silver I] Title: 별 찍기 - 10, Time: 140 ms, Memory: 35196 KB -BaekjoonHub

---
+++ 백준/Silver/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,22 @@
+def _star(arr):
+    res = []
+    length = len(arr)
+    for i in range(length * 3): # 0~9
+        if i // length == 1:
+            res.append(arr[i % length] + ' ' * length + arr[i % length])
+        else:
+            res.append(arr[i % length] * 3)
+    return (list(res))
+
+def star(n):
+    arr = ["***", "* *", "***"]
+    k = 0
+    while (n != 3):
+        n = n // 3
+        k += 1
+    for i in range(k):
+        arr = _star(arr)
+    for i in arr:
+        print(i)
+
+star(int(input()))
