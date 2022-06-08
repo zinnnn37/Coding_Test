914dc7c517992c15d48a8ffe6e5cb50a608da9dc
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2022-06-08 16:04:24 +0900

    [Bronze II] Title: 분해합, Time: 1292 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,6 +1,6 @@
 def d(n):
     for i in range(1, 1000001):
-        arr = list(map(int, list(str(i))))
+        arr = list(map(int, str(i)))
         if (n == i + sum(arr)):
             print(i)
             return
