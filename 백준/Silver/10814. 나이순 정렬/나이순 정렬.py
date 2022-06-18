19df81bba3ac87522d9fb8f6f4c826cae3ac7b60
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2022-06-18 17:13:53 +0900

    [Silver V] Title: 나이순 정렬, Time: 276 ms, Memory: 60828 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,8 +1,9 @@
+import sys
+input = sys.stdin.readline
+
 arr = []
 for _ in range(int(input())):
-    m, n = input().split()
-    arr.append([int(m), n])
-
-arr.sort(key=lambda x : x[0])
+    arr.append(input().split())
+arr.sort(key=lambda x : int(x[0]))
 for data in arr:
-    print(' '.join(map(str, data)))
+    print(' '.join(data))
