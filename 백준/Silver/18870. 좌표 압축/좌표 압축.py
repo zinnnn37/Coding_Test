d9c4a12bed9e03186e5bb077e5f7bb9ffaaa064c
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2022-06-19 13:25:11 +0900

    [Silver II] Title: 좌표 압축, Time: 1776 ms, Memory: 156812 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,7 +1,11 @@
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
+
 n = int(input())
 arr = list(map(int, input().split()))
 s = sorted(list(set(arr)))
 s = {s[i] : i for i in range(len(s))}
 
 for i in arr:
-    print(s[i], end=' ')
+    print(str(s[i]) + ' ')
