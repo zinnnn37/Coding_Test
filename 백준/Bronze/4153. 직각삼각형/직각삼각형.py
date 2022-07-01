0e3f4ee6c558c76fb9d31847446613784fc5300f
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2022-07-01 11:44:06 +0900

    [Bronze III] Title: 직각삼각형, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -4,11 +4,9 @@ input = sys.stdin.readline
 def sol():
     while True:
         arr = sorted(list(map(int, input().split())))
-        if min(arr) == max(arr) == 0:
+        if max(arr) == 0:
             break
-        if min(arr) <= 0:
-            print('wrong')
-        elif pow(arr[0], 2) + pow(arr[1], 2) == pow(arr[2], 2):
+        if arr[0]**2 + arr[1]**2 == arr[2]**2:
             print('right')
         else:
             print('wrong')
