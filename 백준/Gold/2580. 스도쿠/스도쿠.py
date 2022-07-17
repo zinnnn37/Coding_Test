18e606c2febdd85ab9ffa32d2b4024f11b0f213d
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2022-07-17 23:40:44 +0900

    [Gold IV] Title: 스도쿠, Time: 2916 ms, Memory: 145116 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,50 +1,48 @@
 import sys
-input = lambda: sys.stdin.readline().strip()
 
-def check_lines(board, x, y, n):
+board = []
+zero = []
+
+for i in range(9):
+    board.append(list(map(int, sys.stdin.readline().split())))
+    for j in range(9):
+        if board[i][j] == 0:
+            zero.append((i, j))
+
+def check_lines(x, y, n):
     for i in range(9):
         if i != y and board[x][i] == n:
-            return 0
+            return False
         if x != i and board[i][y] == n:
-            return 0
-    return 1
+            return False
+    return True
 
-def check_box(board, x, y, n):
+def check_box(x, y, n):
     a = 3 * (x // 3)
     b = 3 * (y // 3)
     for i in range(a, a + 3):
         for j in range(b, b + 3):
             if (i, j) != (x, y) and board[i][j] == n:
-                return 0
-    return 1
+                return False
+    return True
 
-def dfs(cnt, board, zero):
-    if cnt == len(zero):
-        return 1
-    x, y = zero[cnt]
-    flag = 0
-    for num in range(1, 10):
-        if check_lines(board, x, y, num) and check_box(board, x, y, num):
-            flag = 1
-            board[x][y] = num
-            if dfs(cnt + 1, board, zero) == 0:
-                   board[x][y] = 0
-                   flag = 0
-    return 1 if flag == 1 else 0
+def dfs(i):
+    if i == len(zero):
+        return True
+    
+    x, y = zero[i]
+    flag = False
+    for n in range(1, 10):
+        if check_lines(x, y, n) and check_box(x, y, n):
+            flag = True
+            board[x][y] = n
+            if dfs(i + 1) == 0:
+                board[x][y] = 0
+                flag = False
 
-def print_board(board):
-    for i in range(9):
-        print(*board[i])
+    return True if flag else False
 
-def sol():
-    board = []
-    zero = []
-    for i in range(9):
-        board.append(list(map(int, input().split())))
-        for j in range(9):
-            if board[i][j] == 0:
-                zero.append((i, j))
-    dfs(0, board, zero)
-    print_board(board)
+dfs(0)
 
-sol()
+for i in range(9):
+    print(*board[i])
