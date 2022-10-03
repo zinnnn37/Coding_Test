3908efb83186c6069a00099923010a39b3cc35ea
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2022-10-03 15:55:51 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 668 ms, Memory: 157740 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,27 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10**7)
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+cnt = 1
+
+def dfs(r):
+    global cnt
+    visited[r] = cnt
+    graph[r].sort(reverse=True)
+    for g in graph[r]:
+        if visited[g] == 0:
+            cnt += 1
+            dfs(g)
+
+def sol():
+    for _ in range(m):
+        u, v = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+    dfs(r)
+    print(*visited[1:])
+
+sol()
