b6ed0d2a99606dd1ebb78355b80b250067ed43d4
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2022-10-05 10:38:08 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 612 ms, Memory: 59664 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+cnt = 1
+
+def bfs(r):
+    global cnt
+    q = deque([r])
+    visited[r] = 1
+    while q:
+        v = q.popleft()
+        graph[v].sort(reverse=True)
+        for g in graph[v]:
+            if visited[g] == 0:
+                cnt += 1
+                visited[g] = cnt
+                q.append(g)
+
+def sol():
+    for _ in range(m):
+        u, v = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+    bfs(r)
+    print(*visited[1:])
+
+sol()
