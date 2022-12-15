85373d139923b99a6faff3cdb521f2a26d5de5c7
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2022-12-15 10:13:36 +0900

    [Gold V] Title: 트리, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -2,9 +2,9 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 def dfs(target, arr):
-	arr[target] = -2
+	arr[target] = -2 # 삭제할 노드의 값을 -2로 변경
 	for i in range(n):
-		if target == arr[i]:
+		if target == arr[i]: # 삭제할 노드를 부모 노드로 가지는 자식 노드도 -2로 변경(dfs에서)
 			dfs(i, arr)
 
 if __name__ == '__main__':
@@ -14,6 +14,6 @@ if __name__ == '__main__':
 	cnt = 0
 	dfs(target, arr)
 	for i in range(n):
-		if arr[i] != -2 and i not in arr:
+		if arr[i] != -2 and i not in arr: # 삭제한 노드가 아니며 배열에 없을 경우(부모노드가 아닌 경우)
 			cnt += 1
 	print(cnt)
