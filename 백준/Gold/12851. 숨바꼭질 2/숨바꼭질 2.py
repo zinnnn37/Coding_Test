8ebf117f8edff3aa002df2259953ce6e9e4ef981
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2022-12-27 15:13:33 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 148 ms, Memory: 43144 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -0,0 +1,25 @@
+from collections import deque
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def bfs(s, e):
+	q = deque([s])
+	dp[s][0] = 0;
+	dp[s][1] = 1;
+	while q:
+		cur = q.popleft()
+		for nxt in [cur + 1, cur - 1, cur * 2]:
+			if 0 <= nxt <= 100000:
+				if dp[nxt][0] == -1:
+					dp[nxt][0] = dp[cur][0] + 1
+					dp[nxt][1] = dp[cur][1]
+					q.append(nxt)
+				elif dp[nxt][0] == dp[cur][0] + 1:
+					dp[nxt][1] += dp[cur][1]
+
+if __name__ == '__main__':
+	n, k = map(int, input().split())
+	dp = [[-1, 0] for _ in range(100001)]
+	bfs(n, k)
+	print(dp[k][0])
+	print(dp[k][1])
