450a1bf5f36907fc0a32f5ddf1f8068dfe58d444
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2022-12-28 14:14:00 +0900

    [Silver I] Title: 1로 만들기 2, Time: 1716 ms, Memory: 377724 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -1,25 +1,21 @@
-from collections import deque
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 def to_one(n):
-	q = deque()
-	q.append([n])
-	ans = []
-
-	while q:
-		a = q.popleft()
-		x = a[0]
-		if x == 1:
-			ans = a
-			break
-		if x % 3 == 0:
-			q.append([x // 3] + a)
-		if x % 2 == 0:
-			q.append([x // 2] + a)
-		q.append([x-1] + a)
-	print(len(ans) - 1)
-	print(*ans[::-1])
+	dp = [[0, []] for _ in range(n+1)]
+	dp[1][0] = 0
+	dp[1][1] = [1]
+	for i in range(2, n+1):
+		dp[i][0] = dp[i-1][0] + 1
+		dp[i][1] = dp[i-1][1] + [i]
+		if i % 3 == 0 and dp[i//3][0] + 1 < dp[i][0]:
+			dp[i][0] = dp[i//3][0] + 1
+			dp[i][1] = dp[i//3][1] + [i]
+		if i % 2 == 0 and dp[i//2][0] + 1 < dp[i][0]:
+			dp[i][0] = dp[i//2][0] + 1
+			dp[i][1] = dp[i//2][1] + [i]
+	print(dp[n][0])
+	print(*dp[n][1][::-1])
 
 if __name__ == '__main__':
 	n = int(input())
