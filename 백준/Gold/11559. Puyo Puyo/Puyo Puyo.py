725d69b90cb2c32014cf82b0d587897f0124f015
zinnnn37 <zinnnn37@gmail.com>
2023-01-16 11:49:26 +0900

    [Gold IV] Title: Puyo Puyo, Time: 76 ms, Memory: 34224 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
@@ -1,75 +1,59 @@
+from copy import deepcopy
 from collections import deque
+import sys
+input = lambda: sys.stdin.readline().rstrip()
 
-graph = []
+def bfs(i, j, c):
+	q = deque()
+	q.append((i, j))
+	remove = deque()
+	remove.append((i, j))
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+	visited = [[False] * 6 for _ in range(12)]
+	visited[i][j] = True
+	cnt = 1
+	flag = 0
+	while q:
+		x, y = q.popleft()
+		for i in range(4):
+			nx = x + dx[i]
+			ny = y + dy[i]
+			if 0 <= nx < 12 and 0 <= ny < 6 and puyo[nx][ny] == c and not visited[nx][ny]:
+				q.append((nx, ny))
+				remove.append((nx, ny))
+				visited[nx][ny] = True
+				cnt += 1
+	if cnt >= 4:
+		flag = 1
+		for x, y in remove:
+			puyo[x][y] = '.'
+	return flag
 
-result = 0
+def move():
+	for y in range(6):
+		q = deque()
+		for x in range(11, -1, -1):
+			if puyo[x][y] != '.':
+				q.append(puyo[x][y])
+		for x in range(11, -1, -1):
+			if q:
+				puyo[x][y] = q.popleft()
+			else:
+				puyo[x][y] = '.'
 
-
-def bfs(a, b, c):
-    queue = deque()
-    queue.append((a, b))
-
-    pang = deque()
-    pang.append((a, b))
-
-    visited = [[False] * 6 for _ in range(12)]
-    visited[a][b] = True
-    count = 1
-    flag = 0
-
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx > 11 or ny < 0 or ny > 5:
-                continue
-            if graph[nx][ny] == c and not visited[nx][ny]:
-                queue.append((nx, ny))
-                pang.append((nx, ny))
-                visited[nx][ny] = True
-                count += 1
-
-    if count >= 4:
-        flag = 1
-
-        for x, y in pang:
-            graph[x][y] = "."
-
-    return flag
-
-
-def gravity():
-    for y in range(6):
-        queue = deque()
-        for x in range(11, -1, -1):
-            if graph[x][y] != '.':
-                queue.append(graph[x][y])
-        for x in range(11, -1, -1):
-            if queue:
-                graph[x][y] = queue.popleft()
-            else:
-                graph[x][y] = '.'
-
-
-for i in range(12):
-    graph.append(list(input()))
-
-
-while True:
-    chk = 0
-    for i in range(12):
-        for j in range(6):
-            if graph[i][j] != '.':
-                chk += bfs(i, j, graph[i][j])
-
-    if chk == 0:
-        print(result)
-        break
-    else:
-        result += 1
-    gravity()
+if __name__ == '__main__':
+	dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
+	puyo = [list(input()) for _ in range(12)]
+	ans = 0
+	while True:
+		cnt = 0
+		for i in range(12):
+			for j in range(6):
+				if puyo[i][j] != '.':
+					cnt += bfs(i, j, puyo[i][j])
+		if cnt == 0:
+			print(ans)
+			break
+		else:
+			ans += 1
+		move()
