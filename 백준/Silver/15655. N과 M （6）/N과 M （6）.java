b4e2e20aa39686c110369fcf79751005a211e685
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-02-01 15:58:44 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 16008 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	static int			n;
+	static int			m;
+	static int[]		arr;
+	static int[]		cur;
+	static boolean[]	visited;
+	
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       
+       n = Integer.parseInt(st.nextToken());
+       m = Integer.parseInt(st.nextToken());
+       arr = new int[n];
+       cur = new int[m];
+       visited = new boolean[n];
+       
+       st = new StringTokenizer(br.readLine());
+       for (int i = 0; i < n; i++) {
+    	   arr[i] = Integer.parseInt(st.nextToken());
+       }
+       
+       Arrays.sort(arr);
+       initVisited();
+
+       for (int i = 0; i < n - m + 1; i++) {
+    	   cur[0] = arr[i];
+    	   rec(i, 1);
+       }
+    }
+    
+    private static void rec(int idx, int depth) {
+    	if (depth == m) {
+    		printAns();
+    		return ;
+    	}
+    	
+    	for (int i = idx+1; i < n; i++) {
+    		if (!visited[i]) {
+    			visited[i] = true;
+    			cur[depth] = arr[i];
+    			rec(i, depth + 1);
+    			// 백트래킹
+    			visited[i] = false;
+    		}
+    	}
+    }
+    
+    private static void initVisited() {
+    	for (int i = 0; i < n; i++) {
+    		visited[i] = false;
+    	}
+    }
+    
+    private static void printAns() {
+    	for (int i = 0; i < m; i++) {
+    		System.out.print(cur[i] + " ");
+    	}
+    	System.out.println();
+    }
+}
