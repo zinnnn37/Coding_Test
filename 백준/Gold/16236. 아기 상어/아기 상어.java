3a8634b9c999ebed5ddc2e5a5b1999f54e97307e
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-02-08 01:10:24 +0900

    [Gold III] Title: 아기 상어, Time: 76 ms, Memory: 12180 KB -BaekjoonHub

---
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -0,0 +1,175 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	private static int      N;
+	private static int      ans;
+	private static Shark    shark;
+
+	// considering priority
+	private static final int[]  dx = { -1, 0, 0, 1 };
+	private static final int[]  dy = { 0, -1, 1, 0 };
+
+	private static int[][]      matrix;
+	private static boolean[][]  visited;
+
+	private static Queue<Block>         q;  // bfs
+	private static PriorityQueue<Block> pq; // edible fish
+
+	static class Shark {
+		int x;
+		int y;
+		int size;
+		int eatenFish;
+
+		Shark(int x, int y, int size, int eatenFish) {
+			this.x = x;
+			this.y = y;
+			this.size = size;
+			this.eatenFish = eatenFish;
+		}
+
+		/**
+		 * Update state of shark after eating fish
+		 *
+		 * @param x next row
+		 * @param y next column
+		 */
+		public void updateInfo(int x, int y) {
+			this.x = x;
+			this.y = y;
+			eatenFish++;
+
+			if (eatenFish == size) {
+				size++;
+				eatenFish = 0;
+			}
+		}
+
+	}
+
+	static class Block implements Comparable<Block> {
+		int x;
+		int y;
+		int time;
+
+		Block(int x, int y, int time) {
+			this.x = x;
+			this.y = y;
+			this.time = time;
+		}
+
+		@Override
+		public int compareTo(Block o) {
+			if (this.time != o.time)
+				return Integer.compare(this.time, o.time);
+
+			if (this.x != o.x)
+				return Integer.compare(this.x, o.x);
+
+			return Integer.compare(this.y, o.y);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		init();
+		sol();
+
+	}
+
+	private static void init() throws IOException {
+		BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		N = Integer.parseInt(br.readLine());
+		ans = 0;
+		matrix = new int[N][N];
+		visited = new boolean[N][N];
+
+		q = new ArrayDeque<>();
+		pq = new PriorityQueue<>();
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			for (int j = 0; j < N; j++) {
+				matrix[i][j] = Integer.parseInt(st.nextToken());
+
+				if (matrix[i][j] == 9) {
+					shark = new Shark(i, j, 2, 0);
+					matrix[i][j] = 0;
+				}
+			}
+		}
+	}
+
+	private static void sol() {
+		do {
+			for (int i = 0; i < N; i++) {
+				Arrays.fill(visited[i], false);
+			}
+
+			q.clear();
+			pq.clear();
+
+		} while (findFish());
+
+		System.out.println(ans);
+	}
+
+	/**
+	 * search matrix and find fish that shark can eat
+	 *
+	 * @return  whether edible fish exists or not
+	 */
+	private static boolean findFish() {
+		Block fishToEat = null;
+		int minDist = Integer.MAX_VALUE;
+
+		q.offer(new Block(shark.x, shark.y, 0));
+		visited[shark.x][shark.y] = true;
+
+		while (!q.isEmpty()) {
+			Block b = q.poll();
+
+			if (fishToEat != null && b.time > minDist) break;
+
+			for (int i = 0; i < 4; i++) {
+				int nx = b.x + dx[i];
+				int ny = b.y + dy[i];
+
+				if (!checkOOB(nx, ny) || visited[nx][ny] || shark.size < matrix[nx][ny]) continue;
+
+				visited[nx][ny] = true;
+
+				if (matrix[nx][ny] != 0 && matrix[nx][ny] < shark.size) {
+					if (fishToEat == null) {
+						fishToEat = new Block(nx, ny, b.time + 1);
+						minDist = b.time + 1;
+					} else if (b.time + 1 == minDist) {
+						if (nx < fishToEat.x || (nx == fishToEat.x && ny < fishToEat.y)) {
+							fishToEat = new Block(nx, ny, b.time + 1);
+						}
+					}
+				}
+
+				q.offer(new Block(nx, ny, b.time + 1));
+			}
+		}
+
+		if (fishToEat != null) {
+			matrix[fishToEat.x][fishToEat.y] = 0;
+			shark.updateInfo(fishToEat.x, fishToEat.y);
+			ans += minDist;
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean checkOOB(int x, int y) {
+		return 0 <= x && x < N && 0 <= y && y < N;
+	}
+
+}
