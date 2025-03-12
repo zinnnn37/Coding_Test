2afb9865f05509ca16246804175b34d4c0b4fde2
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-03-12 19:15:00 +0900

    [Gold III] Title: 거울 설치, Time: 80 ms, Memory: 14820 KB -BaekjoonHub

--- 백준/Gold/2151. 거울 설치/거울 설치.java
+++ 백준/Gold/2151. 거울 설치/거울 설치.java
@@ -1,10 +1,8 @@
-import java.awt.Point;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Queue;
 
 public class Main {
@@ -12,27 +10,26 @@ public class Main {
 	private static final int[] re = { 1, 0, 3, 2 };
 	private static final int[] dx = { -1, 1, 0, 0 };
 	private static final int[] dy = { 0, 0, -1, 1 };
-	
-	private static BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	private static int N;
 
 	private static Point start;
 	private static Point end;
-	
-	private static char[][]    house;
+
+	private static char[][] house;
 
 	private static Queue<Node> q;
-	private static Map<Point, Integer> mirror;
 
 	private static class Node {
 		Point p;
-		int dir;
-		int dist;
+		int   dir;
+		int   dist;
 
 		Node(Point p, int dir, int dist) {
-			this.p = p;
-			this.dir = dir;
+			this.p    = p;
+			this.dir  = dir;
 			this.dist = dist;
 		}
 	}
@@ -46,19 +43,15 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 
 		house = new char[N][N];
-		mirror = new HashMap<>();
 		for (int i = 0; i < N; i++) {
 			String s = br.readLine();
 			for (int j = 0; j < N; j++) {
 				house[i][j] = s.charAt(j);
 
-				if (house[i][j] == '!') {
-					mirror.put(new Point(i, j), 100000);
-				} else if (house[i][j] == '#') {
+				if (house[i][j] == '#') {
 					if (start == null) {
 						start = new Point(i, j);
 					} else {
-						mirror.put(new Point(i, j), 100000);
 						end = new Point(i, j);
 					}
 				}
@@ -74,7 +67,7 @@ public class Main {
 	private static void sol() {
 		while (!q.isEmpty()) {
 			Node cur = q.poll();
-			
+
 			if (cur.p.equals(end)) {
 				System.out.println(cur.dist - 1);
 				return;
@@ -83,13 +76,13 @@ public class Main {
 			int cx = cur.p.x;
 			int cy = cur.p.y;
 			int cd = cur.dir;
-			
+
 			for (int i = 0; i < 4; i++) {
 				if (i == cd || isOpposite(i, cd)) continue;
 
 				int nx = cx + dx[i];
 				int ny = cy + dy[i];
-				
+
 				findNextPoint(nx, ny, i, cur.dist);
 			}
 		}
@@ -102,20 +95,18 @@ public class Main {
 			if (house[x][y] == '*') {
 				break;
 			}
-			
+
 			next = new Point(x, y);
-			if ("!#".indexOf(house[x][y]) > -1) {
-				if (mirror.containsKey(next) && mirror.get(next) > dist + 1) {
-					q.add(new Node(next, dir, dist + 1));
-					mirror.put(next, dist + 1);
-				}
+			if (house[x][y] == '!' || house[x][y] == '#') {
+				q.add(new Node(next, dir, dist + 1));
+				house[x][y] = '.';
 			}
-			
+
 			x += dx[dir];
 			y += dy[dir];
 		}
 	}
-	
+
 	private static boolean isOpposite(int i, int d) {
 		return i == re[d];
 	}
