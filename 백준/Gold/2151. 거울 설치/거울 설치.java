5d39c975394740b3f36ac113e7d6672c74bcaf69
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-03-12 17:51:51 +0900

    [Gold III] Title: 거울 설치, Time: 128 ms, Memory: 16832 KB -BaekjoonHub

---
+++ 백준/Gold/2151. 거울 설치/거울 설치.java
@@ -0,0 +1,127 @@
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+
+public class Main {
+
+	private static final int[] re = { 1, 0, 3, 2 };
+	private static final int[] dx = { -1, 1, 0, 0 };
+	private static final int[] dy = { 0, 0, -1, 1 };
+	
+	private static BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+
+	private static int N;
+
+	private static Point start;
+	private static Point end;
+	
+	private static char[][]    house;
+
+	private static Queue<Node> q;
+	private static Map<Point, Integer> mirror;
+
+	private static class Node {
+		Point p;
+		int dir;
+		int dist;
+
+		Node(Point p, int dir, int dist) {
+			this.p = p;
+			this.dir = dir;
+			this.dist = dist;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		init();
+		sol();
+	}
+
+	private static void init() throws IOException {
+		N = Integer.parseInt(br.readLine());
+
+		house = new char[N][N];
+		mirror = new HashMap<>();
+		for (int i = 0; i < N; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < N; j++) {
+				house[i][j] = s.charAt(j);
+
+				if (house[i][j] == '!') {
+					mirror.put(new Point(i, j), 100000);
+				} else if (house[i][j] == '#') {
+					if (start == null) {
+						start = new Point(i, j);
+					} else {
+						mirror.put(new Point(i, j), 100000);
+						end = new Point(i, j);
+					}
+				}
+			}
+		}
+
+		q = new ArrayDeque<>();
+		for (int i = 0; i < 4; i++) {
+			q.add(new Node(start, i, 0));
+		}
+	}
+
+	private static void sol() {
+		while (!q.isEmpty()) {
+			Node cur = q.poll();
+			
+			if (cur.p.equals(end)) {
+				System.out.println(cur.dist - 1);
+				return;
+			}
+
+			int cx = cur.p.x;
+			int cy = cur.p.y;
+			int cd = cur.dir;
+			
+			for (int i = 0; i < 4; i++) {
+				if (i == cd || isOpposite(i, cd)) continue;
+
+				int nx = cx + dx[i];
+				int ny = cy + dy[i];
+				
+				findNextPoint(nx, ny, i, cur.dist);
+			}
+		}
+	}
+
+	private static void findNextPoint(int x, int y, int dir, int dist) {
+		Point next;
+
+		while (!OOB(x, y)) {
+			if (house[x][y] == '*') {
+				break;
+			}
+			
+			next = new Point(x, y);
+			if ("!#".indexOf(house[x][y]) > -1) {
+				if (mirror.containsKey(next) && mirror.get(next) > dist + 1) {
+					q.add(new Node(next, dir, dist + 1));
+					mirror.put(next, dist + 1);
+				}
+			}
+			
+			x += dx[dir];
+			y += dy[dir];
+		}
+	}
+	
+	private static boolean isOpposite(int i, int d) {
+		return i == re[d];
+	}
+
+	private static boolean OOB(int x, int y) {
+		return x < 0 || x >= N || y < 0 || y >= N;
+	}
+
+}
