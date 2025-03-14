d0909b7ee4f95543947a2304361d2e16bacd3b9c
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-03-14 15:38:00 +0900

    [Gold III] Title: 마법사 상어와 파이어스톰, Time: 396 ms, Memory: 86844 KB -BaekjoonHub

---
+++ 백준/Gold/20058. 마법사 상어와 파이어스톰/마법사 상어와 파이어스톰.java
@@ -0,0 +1,177 @@
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int[] dx = { 0, 1, 0, -1 };
+    private static int[] dy = { 1, 0, -1, 0 };
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+
+    private static int N;
+    private static int Q;
+    private static int ans; // total sum of ice
+    private static int cnt; // num of ice
+    private static int len;
+
+    private static int[] cmd;
+
+    private static int[][]     matrix;
+    private static int[][]     copy;
+    private static boolean[][] visited;
+    
+    private static List<Point> toDecrease;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        sol();
+    }
+
+    private static void init() throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken());
+
+        ans = 0;
+        cnt = 0;
+        len = 1 << N;
+
+        matrix = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < len; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+                ans += matrix[i][j];
+            }
+        }
+
+        cmd = new int[Q];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < Q; i++) {
+            cmd[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        visited = new boolean[len][len];
+        toDecrease = new ArrayList<>();
+    }
+
+    private static void sol() {
+        for (int c : cmd) {
+            int pow = 1 << c;
+
+            rotate(pow);
+            meltIce();
+        }
+        getAns();
+    }
+
+    private static void rotate(int pow) {
+        for (int a = 0; a < len / pow; a++) {
+            for (int b = 0; b < len / pow; b++) {
+                copy = new int[pow][pow];
+
+                // make copy
+                for (int i = 0; i < pow; i++) {
+                    for (int j = 0; j < pow; j++) {
+                        // rotate
+                        copy[j][pow - i - 1] = matrix[a * pow + i][b * pow + j];
+                    }
+                }
+                // apply on original mats
+                for (int i = 0; i < pow; i++) {
+                    for (int j = 0; j < pow; j++) {
+                        matrix[a * pow + i][b * pow + j] = copy[i][j];
+                    }
+                }
+            }
+        }
+    }
+
+    private static void meltIce() {
+    	toDecrease.clear();
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (!hasMoreThanThree(i, j)) {
+                	toDecrease.add(new Point(i, j));
+                }
+            }
+        }
+        
+        for (Point p : toDecrease) {
+        	if (matrix[p.x][p.y] <= 0) continue;
+
+        	matrix[p.x][p.y]--;
+        	ans--;
+        }
+    }
+
+    private static boolean hasMoreThanThree(int i, int j) {
+        int cnt = 0;
+
+        for (int d = 0; d < 4; d++) {
+            int nx = i + dx[d];
+            int ny = j + dy[d];
+
+            if (OOB(nx, ny)) {
+                continue;
+            }
+
+            if (matrix[nx][ny] > 0) {
+                cnt++;
+            }
+        }
+        return cnt >= 3;
+    }
+
+    private static void getAns() {
+        // count ice
+        for (int i = 0; i < len; i++) {
+        	for (int j = 0; j < len; j++) {
+        		if (visited[i][j] || matrix[i][j] == 0) continue;
+
+        		cnt = Math.max(cnt, bfs(i, j));
+        	}
+        }
+        System.out.println(ans);
+        System.out.println(cnt);
+    }
+    
+    private static int bfs(int i, int j) {
+    	Queue<Point> q = new ArrayDeque<>();
+    	
+    	q.add(new Point(i, j));
+    	visited[i][j] = true;
+    	
+    	int tmp = 1;
+    	while (!q.isEmpty()) {
+    		Point cur = q.poll();
+    		
+    		for (int d = 0; d < 4; d++) {
+    			int nx = cur.x + dx[d];
+    			int ny = cur.y + dy[d];
+    			
+    			if (OOB(nx, ny) || visited[nx][ny] || matrix[nx][ny] == 0) continue;
+    			
+    			q.offer(new Point(nx, ny));
+    			visited[nx][ny] = true;
+    			tmp++;
+    		}
+    	}
+    	return tmp;
+    }
+
+    private static boolean OOB(int x, int y) {
+        return x < 0 || len <= x || y < 0 || len <= y;
+    }
+
+}
