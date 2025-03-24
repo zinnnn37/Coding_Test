3cdc68de7fed7a89f159f6991f01a97f4c9532c6
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-03-24 15:29:27 +0900

    [Silver V] Title: 숫자 카드, Time: 780 ms, Memory: 173324 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,7 +13,8 @@ public class Main {
 
     private static int N;
     private static int M;
-    private static int[] cards;
+
+    private static Set<Integer> set;
 
     public static void main(String[] args) throws IOException {
         init();
@@ -21,13 +23,12 @@ public class Main {
 
     private static void init() throws IOException {
         N = Integer.parseInt(br.readLine());
-
-        cards = new int[N];
         st = new StringTokenizer(br.readLine());
+
+        set = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            cards[i] = Integer.parseInt(st.nextToken());
+            set.add(Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(cards);
     }
 
     private static void sol() throws IOException {
@@ -37,24 +38,9 @@ public class Main {
         for (int i = 0; i < M; i++) {
             int target = Integer.parseInt(st.nextToken());
 
-            sb.append(binarySearch(0, N - 1, target)).append(" ");
+            sb.append(set.contains(target) ? "1 " : "0 ");
         }
         System.out.println(sb);
-    }
-
-    private static int binarySearch(int start, int end, int target) {
-        while (start <= end) {
-            int mid = (start + end) / 2;
-
-            if (cards[mid] == target) {
-                return 1;
-            } else if (cards[mid] < target) {
-            	start = mid + 1;
-            } else {
-            	end = mid - 1;
-            }
-        }
-        return 0;
     }
 
 }
