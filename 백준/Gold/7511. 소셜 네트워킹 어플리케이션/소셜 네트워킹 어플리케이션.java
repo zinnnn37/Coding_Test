665c9609bb6a92e7358e2522c95d06df49283797
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-04-10 12:03:35 +0900

    [Gold V] Title: 소셜 네트워킹 어플리케이션, Time: 876 ms, Memory: 173944 KB -BaekjoonHub

---
+++ 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final StringBuilder sb = new StringBuilder();
+    private static StringTokenizer st;
+
+    private static int N; // users
+    private static int K; // relations
+    private static int M;
+
+    private static int[] parents;
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine().trim());
+
+        for (int tc = 1; tc <= T; tc++) {
+            sb.append("Scenario ").append(tc).append(":\n");
+            sol();
+        }
+        System.out.println(sb);
+    }
+
+    private static void sol() throws IOException {
+        N = Integer.parseInt(br.readLine().trim());
+        K = Integer.parseInt(br.readLine().trim());
+
+        parents = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parents[i] = i;
+        }
+
+        while (K-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            union(a, b);
+        }
+
+        M = Integer.parseInt(br.readLine().trim());
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(find(a) == find(b) ? "1\n" : "0\n");
+        }
+        sb.append("\n");
+    }
+
+    private static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if (rootA < rootB) {
+            parents[rootB] = rootA;
+        } else {
+            parents[rootA] = rootB;
+        }
+    }
+
+    private static int find(int a) {
+        if (parents[a] == a) {
+            return a;
+        }
+        return find(parents[a]);
+    }
+
+}
