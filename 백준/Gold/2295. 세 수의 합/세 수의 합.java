4b26428342ff80fa20859e9074957700844dd06a
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-04-21 17:17:05 +0900

    [Gold IV] Title: 세 수의 합, Time: 264 ms, Memory: 53120 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -2,68 +2,54 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-	static int[] nums;
-	static int[] sumOfTwo;
-	static int len;
+
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	private static int   N;
+	private static int[] nums;
+
+	private static Set<Integer> pref;
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		nums = new int[n];
-		len = n;
-		
-		for (int i = 0; i < n; i++) {
+		init();
+		sol();
+	}
+
+	private static void init() throws IOException {
+		N = Integer.parseInt(br.readLine());
+
+		nums = new int[N];
+		for (int i = 0; i < N; i++) {
 			nums[i] = Integer.parseInt(br.readLine());
 		}
-		
-		int idx = 0;
-		sumOfTwo = new int[(len * (len+1)) / 2];
-		for (int i = 0; i < len; i++) {
-			for (int j = i; j < len; j++) {
-				sumOfTwo[idx++] = nums[i] + nums[j];
-			}
-		}
-		
-		Arrays.sort(sumOfTwo);
-		int ans = sumOfThree();
+		Arrays.sort(nums);
 
-		System.out.println(ans);
+		pref = new HashSet<>();
 	}
-	
-	static boolean binarySearch(int target) {
-		int start = 0;
-		int end = sumOfTwo.length - 1;
-		
-		while (start <= end) {
-			int mid = start + (end - start) / 2;
-			
-			if (sumOfTwo[mid] < target) {
-				start = mid + 1;
-			} else if (sumOfTwo[mid] > target) {
-				end = mid - 1;
-			} else {
-				return true;
+
+	private static void sol() {
+		// a + b
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				pref.add(nums[i] + nums[j]);
 			}
 		}
-		
-		return false;
-	}
-	
-	static int sumOfThree() {
-		int ans = -1;
-		for (int i = 0; i < len; i++) {
-			for (int j = i; j < len; j++) {
-				if (binarySearch(nums[j] - nums[i])) {
-					ans = Math.max(ans,  nums[j]);
+
+		// k - c
+		for (int i = N - 1; i >= 0; i--) {
+			for (int j = 0; j < i; j++) {
+				int target = nums[i] - nums[j];
+
+				if (pref.contains(target)) {
+					System.out.println(nums[i]);
+					System.exit(0);
 				}
 			}
 		}
-		return ans;
 	}
 
 }
