08e978b5dafe98dc032824fa8ccdbbeea25bc427
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-09-14 12:18:33 +0900

    [Gold V] Title: 0 만들기, Time: 84 ms, Memory: 13264 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -1,19 +1,17 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
-	private static final char[] OPS = { ' ', '+', '-' };
+	private static final String OPS = " +-";
 
 	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	private static final StringBuilder  sb = new StringBuilder();
 
-	private static int          N;
-	private static char[]       ops;
-	private static List<String> res;
+	private static int    N;
+	private static char[] ops;
 
 	public static void main(String[] args) throws IOException {
 		int T = Integer.parseInt(br.readLine());
@@ -21,10 +19,10 @@ public class Main {
 
 		while (T-- > 0) {
 			sol();
-
-			if (T > 0) sb.append('\n');
+			if (T > 0) {
+				sb.append("\n");
+			}
 		}
-
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
@@ -32,69 +30,66 @@ public class Main {
 	}
 
 	private static void sol() throws IOException {
-		N   = Integer.parseInt(br.readLine());
-		res = new ArrayList<>();
+		N = Integer.parseInt(br.readLine());
 
-		dfs(1);
-
-		Collections.sort(res);
-		for (String result : res) {
-			sb.append(result).append('\n');
+		for (int i = 0; i < 3; i++) {
+			char o = OPS.charAt(i);
+			ops[2] = o;
+			dfs(2);
 		}
 	}
 
 	private static void dfs(int num) {
 		if (num == N) {
-			String expression = appendString();
-			if (evaluate(expression) == 0) {
-				res.add(expression);
+			if (isValid()) {
+				appendString();
 			}
 			return;
 		}
 
-		for (char op : OPS) {
-			ops[num] = op;
+		for (int i = 0; i < 3; i++) {
+			char o = OPS.charAt(i);
+			ops[num + 1] = o;
 			dfs(num + 1);
 		}
 	}
 
-	private static String appendString() {
-		StringBuilder exp = new StringBuilder();
-		for (int i = 1; i <= N; i++) {
-			exp.append(i);
-			if (i < N) {
-				exp.append(ops[i]);
+	private static boolean isValid() {
+		List<Integer> nums = new ArrayList<>();
+
+		// 공백 처리
+		int idx = 0;
+		while (++idx <= N) {
+			int num = idx;
+			while (idx < N && ops[idx + 1] == ' ') {
+				idx++;
+				num = num * 10 + idx;
 			}
+			nums.add(num);
 		}
-		return exp.toString();
+
+		int res   = nums.get(0);
+		int opIdx = 2;
+
+		for (int i = 1; i < nums.size(); i++) {
+			// 공백 건너뛰기
+			while (opIdx <= N && ops[opIdx] == ' ') opIdx++;
+
+			if (ops[opIdx] == '+') {
+				res += nums.get(i);
+			} else {
+				res -= nums.get(i);
+			}
+			opIdx++;
+		}
+
+		return res == 0;
 	}
 
-	private static int evaluate(String expression) {
-		String noBlank = expression.replaceAll(" ", "");
-
-		int  result = 0;
-		int  cn     = 0;
-		char co     = '+';
-
-		for (int i = 0; i < noBlank.length(); i++) {
-			char c = noBlank.charAt(i);
-
-			// 숫자 합치기
-			if (Character.isDigit(c)) {
-				cn = cn * 10 + (c - '0');
-			}
-
-			if (c == '+' || c == '-' || i == noBlank.length() - 1) {
-				if (co == '+') {
-					result += cn;
-				} else {
-					result -= cn;
-				}
-				co = c;
-				cn = 0;
-			}
+	private static void appendString() {
+		for (int i = 1; i < N; i++) {
+			sb.append(i).append(ops[i + 1]);
 		}
-
-		return result;
+		sb.append(N).append('\n');
 	}
 }
