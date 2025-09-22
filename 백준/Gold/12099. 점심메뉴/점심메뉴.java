b5baf1a319f90d5c86f9a066206d212fe72b2617
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-09-22 21:10:19 +0900

    [Gold IV] Title: 점심메뉴, Time: 2132 ms, Memory: 140884 KB -BaekjoonHub

--- 백준/Gold/12099. 점심메뉴/점심메뉴.java
+++ 백준/Gold/12099. 점심메뉴/점심메뉴.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,8 +12,23 @@ public class Main {
 	private static int N;
 	private static int Q;
 
-	private static int[] spiciness;
-	private static int[] sweetness;
+	private static Node[] spiciness;
+	private static int[]  sweetness;
+
+	private static class Node implements Comparable<Node> {
+		int spiciness;
+		int sweetness;
+
+		Node(int s, int w) {
+			spiciness = s;
+			sweetness = w;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.spiciness, o.spiciness);
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		init();
@@ -24,17 +40,17 @@ public class Main {
 		N  = Integer.parseInt(st.nextToken());
 		Q  = Integer.parseInt(st.nextToken());
 
-		spiciness = new int[N];
-		sweetness = new int[N];
+		spiciness = new Node[N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 
 			int s = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 
-			spiciness[i] = s;
-			sweetness[i] = w;
+			spiciness[i] = new Node(s, w);
 		}
+
+		Arrays.sort(spiciness);
 	}
 
 	private static void sol() throws IOException {
@@ -46,17 +62,98 @@ public class Main {
 			int w1 = Integer.parseInt(st.nextToken());
 			int w2 = Integer.parseInt(st.nextToken());
 
-			int cnt = 0;
-			for (int i = 0; i < N; i++) {
-				if (s1 <= spiciness[i] && spiciness[i] <= s2 && w1 <= sweetness[i] && sweetness[i] <= w2) {
-					cnt++;
-				}
+			int left  = lowerBound(s1);
+			int right = upperBound(s2);
+
+			if (left >= right) {
+				bw.write("0\n");
+				continue;
 			}
-			bw.write(cnt + "\n");
+
+			int len = right - left;
+
+			sweetness = new int[len];
+			for (int i = left; i < right; i++) {
+				sweetness[i - left] = spiciness[i].sweetness;
+			}
+			Arrays.sort(sweetness);
+
+			int sweetLeft  = lowerBoundSweetness(w1, len);
+			int sweetRight = upperBoundSweetness(w2, len);
+
+			bw.write((sweetRight - sweetLeft) + "\n");
 		}
 		bw.flush();
 		bw.close();
 		br.close();
+	}
+
+	// s1 이상인 첫 번째 인덱스
+	private static int lowerBound(int target) {
+		int left  = 0;
+		int right = N;
+
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+
+			if (spiciness[mid].spiciness < target) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return left;
+	}
+
+	// s2 초과인 첫 번째 인덱스
+	private static int upperBound(int target) {
+		int left  = 0;
+		int right = N;
+
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+
+			if (spiciness[mid].spiciness <= target) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return left;
+	}
+
+	// w1 이상인 첫 번째 인덱스
+	private static int lowerBoundSweetness(int target, int len) {
+		int left  = 0;
+		int right = len;
+
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+
+			if (sweetness[mid] < target) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return left;
+	}
+
+	// w2 초과인 첫 번째 인덱스
+	private static int upperBoundSweetness(int target, int len) {
+		int left  = 0;
+		int right = len;
+
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+
+			if (sweetness[mid] <= target) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return left;
 	}
 
 }
