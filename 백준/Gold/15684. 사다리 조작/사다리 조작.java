7155e75eba01e4c45f25074e7bf26b0e09e35a6b
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-10-22 20:09:54 +0900

    [Gold III] Title: 사다리 조작, Time: 408 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	private static final BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+	private static final BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	private static       StringTokenizer st;
+
+	private static int N, M, H;
+	private static boolean[][] ladder;
+
+	public static void main(String[] args) throws IOException {
+		init();
+
+		// 3개까지만 확인
+		for (int target = 0; target <= 3; target++) {
+			if (dfs(1, 0, target)) {
+				bw.write(target + "");
+				bw.flush();
+				bw.close();
+				br.close();
+				return;
+			}
+		}
+
+		bw.write("-1");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	private static void init() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+
+		ladder = new boolean[H + 1][N + 1];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			ladder[a][b] = true;
+		}
+	}
+
+	private static boolean dfs(int h, int cnt, int target) {
+		if (cnt == target) {
+			return checkValid();
+		}
+
+		for (int i = h; i <= H; i++) {
+			for (int j = 1; j < N; j++) {
+				if (canPlace(i, j)) {
+					ladder[i][j] = true;
+					if (dfs(i, cnt + 1, target)) {
+						return true;
+					}
+					ladder[i][j] = false;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static boolean canPlace(int h, int n) {
+		if (ladder[h][n]) {
+			return false;
+		} else if (n > 1 && ladder[h][n - 1]) {
+			return false;
+		} else if (n < N - 1 && ladder[h][n + 1]) {
+			return false;
+		}
+		return true;
+	}
+
+	private static boolean checkValid() {
+		for (int start = 1; start <= N; start++) {
+			int pos = start;
+
+			for (int h = 1; h <= H; h++) {
+				if (ladder[h][pos]) {
+					pos++;  // 오른쪽으로 이동
+				} else if (pos > 1 && ladder[h][pos - 1]) {
+					pos--;  // 왼쪽으로 이동
+				}
+			}
+			if (pos != start) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
