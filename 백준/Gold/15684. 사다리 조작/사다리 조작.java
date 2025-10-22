c0c3dfe6de9a905cd38562da3b2b7251ceac890f
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-10-22 20:12:36 +0900

    [Gold III] Title: 사다리 조작, Time: 336 ms, Memory: 13772 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.java
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -12,10 +12,9 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		init();
-
-		// 3개까지만 확인
+		
 		for (int target = 0; target <= 3; target++) {
-			if (dfs(1, 0, target)) {
+			if (dfs(1, 1, 0, target)) {
 				bw.write(target + "");
 				bw.flush();
 				bw.close();
@@ -23,7 +22,7 @@ public class Main {
 				return;
 			}
 		}
-
+		
 		bw.write("-1");
 		bw.flush();
 		bw.close();
@@ -39,55 +38,52 @@ public class Main {
 		ladder = new boolean[H + 1][N + 1];
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-
 			ladder[a][b] = true;
 		}
 	}
 
-	private static boolean dfs(int h, int cnt, int target) {
+	private static boolean dfs(int h, int n, int cnt, int target) {
 		if (cnt == target) {
-			return checkValid();
+			return check();
 		}
-
+		
 		for (int i = h; i <= H; i++) {
-			for (int j = 1; j < N; j++) {
+			int colStart = (i == h ? n : 1);
+			for (int j = colStart; j < N; j++) {
 				if (canPlace(i, j)) {
 					ladder[i][j] = true;
-					if (dfs(i, cnt + 1, target)) {
+					if (dfs(i, j + 2, cnt + 1, target)) {
 						return true;
 					}
 					ladder[i][j] = false;
 				}
 			}
 		}
+		
 		return false;
 	}
 
 	private static boolean canPlace(int h, int n) {
-		if (ladder[h][n]) {
-			return false;
-		} else if (n > 1 && ladder[h][n - 1]) {
-			return false;
-		} else if (n < N - 1 && ladder[h][n + 1]) {
-			return false;
-		}
+		if (ladder[h][n]) return false;
+		if (n > 1 && ladder[h][n - 1]) return false;
+		if (n < N - 1 && ladder[h][n + 1]) return false;
 		return true;
 	}
 
-	private static boolean checkValid() {
+	private static boolean check() {
 		for (int start = 1; start <= N; start++) {
 			int pos = start;
-
+			
 			for (int h = 1; h <= H; h++) {
-				if (ladder[h][pos]) {
-					pos++;  // 오른쪽으로 이동
+				if (pos < N && ladder[h][pos]) {
+					pos++;
 				} else if (pos > 1 && ladder[h][pos - 1]) {
-					pos--;  // 왼쪽으로 이동
+					pos--;
 				}
 			}
+			
 			if (pos != start) {
 				return false;
 			}
