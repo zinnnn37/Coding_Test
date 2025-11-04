996db92bf5887b7e4dfc76cd791f01aadce50871
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-11-04 23:24:39 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 108 ms, Memory: 15860 KB -BaekjoonHub

--- 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
@@ -1,7 +1,5 @@
 import java.io.*;
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,27 +8,12 @@ public class Main {
 	private static final BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	private static       StringTokenizer st;
 
-	private static int N, K, nxt;
-	private static int[]       minCnt;
-	private static Queue<Node> q;
-
-	private static class Node {
-		int step;
-		int cnt;
-
-		Node(int step, int cnt) {
-			this.step = step;
-			this.cnt = cnt;
-		}
-	}
+	private static int N, K;
+	private static int[] dp;
 
 	public static void main(String[] args) throws IOException {
 		init();
 		sol();
-
-		bw.flush();
-		bw.close();
-		br.close();
 	}
 
 	private static void init() throws IOException {
@@ -38,40 +21,23 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
-		minCnt = new int[N + 1];
-		Arrays.fill(minCnt, Integer.MAX_VALUE);
-
-		q = new ArrayDeque<>();
-		q.offer(new Node(0, 0));
-		minCnt[0] = 0;
+		dp = new int[N + 1];
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		dp[0] = 0;
 	}
 
 	private static void sol() throws IOException {
-		while (!q.isEmpty()) {
-			Node cur = q.poll();
+		for (int i = 0; i < N; i++) {
+			dp[i + 1] = Math.min(dp[i + 1], dp[i] + 1);
 
-			if (cur.step == N) {
-				bw.write("minigimbob");
-				return;
-			}
-
-			if (cur.cnt >= K) {
-				continue;
-			}
-
-			nxt = cur.step + 1;
-			if (nxt <= N && minCnt[nxt] > cur.cnt + 1) {
-				minCnt[nxt] = cur.cnt + 1;
-				q.offer(new Node(nxt, cur.cnt + 1));
-			}
-
-			nxt = cur.step + cur.step / 2;
-			if (nxt <= N && minCnt[nxt] > cur.cnt + 1) {
-				minCnt[nxt] = cur.cnt + 1;
-				q.offer(new Node(nxt, cur.cnt + 1));
-			}
+			int next = i + i / 2;
+			if (i < next && next <= N) dp[next] = Math.min(dp[next], dp[i] + 1);
 		}
-		bw.write("water");
+
+		bw.write(dp[N] <= K ? "minigimbob" : "water");
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
 }
