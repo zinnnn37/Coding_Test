bd6c5a27b4a914ee483153f7a3795e412e695924
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-11-10 20:03:55 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.17 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,6 +1,60 @@
+import java.util.*;
+
 class Solution {
-    public int solution(int n, int[][] wires) {
-        int answer = -1;
-        return answer;
+    
+    private static int N, ans;
+    private static int[][] wires;
+    private static boolean[] visited;
+    private static List<Integer>[] graph;
+    
+    private static void init(int n, int[][] wire) {
+        N = n;
+        ans = Integer.MAX_VALUE;
+        wires = wire;
+        
+        visited = new boolean[N+1];
+        
+        graph = new List[N+1];
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for (int[] edge : wires) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
     }
+    
+    private static void sol() {
+        int cnt;
+        int left;
+        for (int i = 0; i < N-1; i++) {
+            Arrays.fill(visited, false);
+            
+            visited[wires[i][0]] = true;
+            visited[wires[i][1]] = true;
+            cnt = exclude(wires[i][0], wires[i], 0);
+            
+            left = N - cnt;
+            ans = Math.min(ans, Math.abs(cnt - left));
+        }
+    }
+    
+    private static int exclude(int start, int[] target, int cnt) {
+        for (int n : graph[start]) {
+            if (visited[n]) continue;
+            
+            visited[n] = true;
+            cnt += exclude(n, target, 0);
+        }
+        return cnt + 1;
+    }
+    
+    public int solution(int n, int[][] wires) {
+        init(n, wires);
+        sol();
+        
+        return ans;
+    }
+    
 }
