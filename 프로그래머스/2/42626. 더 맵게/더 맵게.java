03544bc39ba9f043df957a753f1c856a521742e0
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-11-19 23:35:53 +0900

    [level 2] Title: 더 맵게, Time: 1719.17 ms, Memory: 129 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    
+    private static Queue<Integer> pq;
+    
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        pq = new PriorityQueue<>();
+        
+        for (int s : scoville) {
+            pq.offer(s);
+        }
+        
+        while (!pq.isEmpty() && pq.peek() < K) {
+            if (pq.size() < 2) return -1;
+            
+            int a = pq.poll();
+            int b = pq.poll();
+            
+            pq.offer(a + 2 * b);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
