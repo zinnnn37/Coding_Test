81e097d81be3be866a2b1a69f131c95cb5f72ce8
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-11-21 23:52:35 +0900

    [Gold V] Title: 빗물, Time: 68 ms, Memory: 11808 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static       StringTokenizer st;
+
+    private static int   H, W;
+    private static int[] height;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        sol();
+    }
+
+    private static void init() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        H = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+
+        height = new int[W];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < W; i++) {
+            height[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    private static void sol() throws IOException {
+        int left = 0, right = W - 1;
+        int leftMax = 0, rightMax = 0;
+        int result = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    result += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    result += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
