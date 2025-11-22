4f6b091faab9bb5d047a73cc3018c7d85b326acf
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-11-22 19:55:06 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2040 ms, Memory: 894284 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder   sb = new StringBuilder();
+    private static       StringTokenizer st;
+
+    private static int N, L;
+    private static int[]            nums;
+    private static ArrayDeque<Node> dq;
+
+    private static class Node {
+
+        int idx;
+        int val;
+
+        Node(int idx, int val) {
+            this.idx = idx;
+            this.val = val;
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+        sol();
+    }
+
+    private static void init() throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+
+        nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        dq = new ArrayDeque<>();
+    }
+
+    private static void sol() throws IOException {
+        for (int i = 0; i < N; i++) {
+            while (!dq.isEmpty() && dq.getLast().val >= nums[i]) dq.removeLast();
+
+            dq.offer(new Node(i, nums[i]));
+
+            if (dq.getFirst().idx == i - L) dq.removeFirst();
+
+            sb.append(dq.getFirst().val).append(' ');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
