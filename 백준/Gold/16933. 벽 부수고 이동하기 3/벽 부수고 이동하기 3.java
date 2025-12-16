54e264cc90fe88cec11c0b3925f8304ac6060d59
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2025-12-16 23:11:48 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 3160 ms, Memory: 337224 KB -BaekjoonHub

---
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -0,0 +1,107 @@
+import java.io.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final int[] dx = { 0, -1, 0, 1 };
+    private static final int[] dy = { -1, 0, 1, 0 };
+
+    private static BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+
+    private static int N, M, K;
+    private static char[][]      matrix;
+    private static boolean[][][] visited;
+    private static Queue<Node>   q;
+
+    private static class Node implements Comparable<Node> {
+        int     x;
+        int     y;
+        int     k;
+        int     cnt;
+        boolean isDay;
+
+        public Node(int x, int y, int k, int cnt, boolean isDay) {
+            this.x = x;
+            this.y = y;
+            this.k = k;
+            this.cnt = cnt;
+            this.isDay = isDay;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.cnt, o.cnt);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+        sol();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void init() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        matrix = new char[N][M];
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                matrix[i][j] = input.charAt(j);
+            }
+        }
+        visited = new boolean[N][M][K + 1];
+        q = new PriorityQueue<>();
+    }
+
+    private static void sol() throws IOException {
+        q.offer(new Node(0, 0, 0, 1, true));
+        visited[0][0][0] = true;
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+
+            if (cur.x == N - 1 && cur.y == M - 1) {
+                bw.write(cur.cnt + "");
+                return;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nx = cur.x + dx[d];
+                int ny = cur.y + dy[d];
+
+                if (OOB(nx, ny)) continue;
+
+                if (!visited[nx][ny][cur.k] && matrix[nx][ny] == '0') {
+                    visited[nx][ny][cur.k] = true;
+                    q.offer(new Node(nx, ny, cur.k, cur.cnt + 1, !cur.isDay));
+                } else if (cur.k < K && !visited[nx][ny][cur.k + 1] && matrix[nx][ny] == '1') {
+                    if (cur.isDay) {
+                        visited[nx][ny][cur.k + 1] = true;
+                        q.offer(new Node(nx, ny, cur.k + 1, cur.cnt + 1, false));
+                    } else {
+                        visited[nx][ny][cur.k + 1] = true;
+                        q.offer(new Node(nx, ny, cur.k + 1, cur.cnt + 2, false));
+                    }
+                }
+            }
+        }
+        bw.write("-1");
+    }
+
+    private static boolean OOB(int x, int y) {
+        return x < 0 || N <= x || y < 0 || M <= y;
+    }
+
+}
