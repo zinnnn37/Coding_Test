06cd2e1356f1ebd1a301b9a445be15f40e890155
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2026-01-29 18:24:02 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 4456 ms, Memory: 152468 KB -BaekjoonHub

---
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static       StringTokenizer st;
+
+    private static int   N;
+    private static long  cnt;
+    private static int[] A, B, C, D;
+    private static int[] AB, CD;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        sol();
+    }
+
+    private static void init() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        cnt = 0;
+
+        A = new int[N];
+        B = new int[N];
+        C = new int[N];
+        D = new int[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            A[i] = Integer.parseInt(st.nextToken());
+            B[i] = Integer.parseInt(st.nextToken());
+            C[i] = Integer.parseInt(st.nextToken());
+            D[i] = Integer.parseInt(st.nextToken());
+        }
+
+        AB = new int[N * N];
+        CD = new int[N * N];
+    }
+
+    private static void sol() throws IOException {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                AB[i * N + j] = A[i] + B[j];
+                CD[i * N + j] = C[i] + D[j];
+            }
+        }
+        Arrays.sort(AB);
+        Arrays.sort(CD);
+
+        int left  = 0;
+        int right = N * N - 1;
+        while (left < N * N && right >= 0) {
+            int sum = AB[left] + CD[right];
+
+            if (sum == 0) {
+                int lVal = AB[left];
+                int rVal = CD[right];
+
+                long lCnt = 1;
+                long rCnt = 1;
+                while (++left < N * N && AB[left] == lVal) {
+                    lCnt++;
+                }
+                while (--right >= 0 && CD[right] == rVal) {
+                    rCnt++;
+                }
+                cnt += (long) lCnt * rCnt;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        bw.write(cnt + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
