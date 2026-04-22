ef0fe8358a2a34d8a04351a754811e4be8d74fee
zinnnn37 <102711874+zinnnn37@users.noreply.github.com>
2026-04-22 14:22:22 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,9 @@
+-- 코드를 작성해주세요
+SELECT DISTINCT(ID) AS ID,
+       EMAIL,
+       FIRST_NAME,
+       LAST_NAME
+  FROM DEVELOPERS D, SKILLCODES C
+ WHERE D.SKILL_CODE & C.CODE
+    AND (C.NAME = 'C#' OR C.NAME = 'Python')
+ ORDER BY ID;
